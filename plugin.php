0bb4f5ba13f6f66328deae838a44a50d2f11815b
Joachim Happel <joachim.happel@gmail.com>
2017-01-16 11:23:25 +0100

    initial
    uses single standalone recaptcha plugin from
    https://github.com/osTicket/osTicket-plugins

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'id' =>             'osticket:reCAPTCHA', 
+    'version' =>        '0.1',
+    'name' =>           'Google reCAPTCHA field',
+    'author' =>         'Jared Hancock',
+    'description' =>    'Provides a reCAPTCHA field which can be added to the contact information. This will help verify humans for both new tickets by guests as well as client registration',
+    'url' =>            'http://www.osticket.com/plugins/field/recaptcha',
+    'plugin' =>         'field.php:reCaptchaPlugin',
+);
